0bd7a5e94f27d246125b24c9fe20fb3fd3756800
silpa-nallani <nallani.silpa@hcl.com>
2026-09-07 17:15:54 +0530

    resolved error

--- src/main/java/com/github/actions/Controller/TestController.java
+++ src/main/java/com/github/actions/Controller/TestController.java
@@ -2,6 +2,8 @@
 package com.github.actions.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 @RestController("/githubactions")
 public class TestController {
 
