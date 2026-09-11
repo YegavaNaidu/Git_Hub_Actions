c4a49819e8725b32b38b9a62fc21d02e20342a72
silpa-nallani <nallani.silpa@hcl.com>
2026-09-11 16:26:22 +0530

    modified rest controller path

--- src/main/java/com/github/actions/Controller/TestController.java
+++ src/main/java/com/github/actions/Controller/TestController.java
@@ -1,17 +1,18 @@
 
-package com.github.actions.Controller;
+  package com.github.actions.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-@RestController("/githubactions")
-public class TestController {
-
-
-@GetMapping("/test1")
-public String test1(){
-  return "my first deployment in azure VM through github Actions done successfully";
-}
+  import org.springframework.web.bind.annotation.RestController;
+  import org.springframework.web.bind.annotation.GetMapping;
+  import org.springframework.web.bind.annotation.RequestMapping;
+  @RestController()
+  @RequestMapping("/api")
+  public class TestController {
 
 
-}
+  @GetMapping("/test1")
+  public String test1(){
+    return "my first deployment in azure VM through github Actions done successfully";
+  }
+
+
+  }
